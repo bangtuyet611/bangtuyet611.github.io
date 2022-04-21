9d92fb4f05392ae8d3e40b1bfaff9897b0aa5e29
bangtuyet611 <103877364+bangtuyet611@users.noreply.github.com>
2022-04-22 00:23:55 +0700

    commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $(window).scroll(() => {
-        $('.search-form-tuyet').removeClass('active') // neu lan chuot thi an thanh tim kiem
+        // neu lan chuot thi an thanh tim kiem
+        $('.search-form-tuyet').removeClass('active') 
         // navbar nam tren cung neu lan chuot
         if ($(window).scrollTop() > 80 ) {
             $('.header-tuyet .header-2-tuyet').addClass('active');
--- js/script.js
+++ js/script.js
@@ -17,3 +17,52 @@ var swiper = new Swiper(".books-slider-tuyet", {
         },
     },
 });
+
+var swiper = new Swiper(".featured-slider-tuyet", {
+    spaceBetween: 10,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        450: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 4,
+        },
+    },
+});
+
+var swiper = new Swiper(".book-slider-tuyet", {
+    spaceBetween: 10,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
