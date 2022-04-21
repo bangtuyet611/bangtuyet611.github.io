1c018dc14f158b7b48785c9dbf93d8aa29f5c18d
bangtuyet611 <103877364+bangtuyet611@users.noreply.github.com>
2022-04-21 07:03:57 +0700

    commit

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,4 @@
 $(document).ready(function() {
-    $(document).on("click", ".icon-tuyet", function() {
-        var classNav = $("#myTopnav-tuyet").attr("class");
-        console.log(classNav);
-        if (classNav === "topnav-tuyet")
-            $("#myTopnav-tuyet").addClass("responsive-tuyet");
-        else 
-        $("#myTopnav-tuyet").removeClass("responsive-tuyet");
-    });
-
     $(document).on('click', 'a[href^="#"]', function (event) {
         event.preventDefault();
     
@@ -15,25 +6,36 @@ $(document).ready(function() {
             scrollTop: $($.attr(this, 'href')).offset().top
         }, 500);
     });
+
+    $(document).on("click", "#khoa-hoc", function() {
+      $('img').each(function(){
+        if ($(this).attr('id') == 'book-sample') {
+          $(this).attr('src', './img/book-sample-3.png');
+        }
+      });
+    });
+
+    $(document).on("click", "#tieu-thuyet", function() {
+      $('img').each(function(){
+        if ($(this).attr('id') == 'book-sample') {
+          $(this).attr('src', './img/book-sample-2.png');
+        }
+      });
+    });
+
+    $(document).on("click", "#van-hoc", function() {
+      $('img').each(function(){
+        if ($(this).attr('id') == 'book-sample') {
+          $(this).attr('src', './img/book-sample-4.png');
+        }
+      });
+    });
+
+    $(document).on("click", "#thieu-nhi", function() {
+      $('img').each(function(){
+        if ($(this).attr('id') == 'book-sample') {
+          $(this).attr('src', './img/book-sample-5.png');
+        }
+      });
+    });
 });
-
-
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("slide-tuyet");
-  let dots = document.getElementsByClassName("dot-tuyet");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
